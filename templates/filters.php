51c421b73bec49e9eb2e2d89169948896bda3399
ferranrecio <ferran@moodle.com>
2024-04-18 11:42:59 +0200

    Adding filters

---
+++ templates/filters.php
@@ -0,0 +1,23 @@
+<div class="d-flex gap-3 ps-2" id="instanceFilters">
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="filter" id="all" value="" checked>
+    <label class="form-check-label" for="all">All</label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="filter" id="MDL" value="MDL">
+    <label class="form-check-label" for="MDL">MDL</label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="filter" id="int" value="int">
+    <label class="form-check-label" for="int">Integration</label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="filter" id="stable" value="stable">
+    <label class="form-check-label" for="stable">Stable</label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="radio" name="filter" id="other" value="other">
+    <label class="form-check-label" for="other">Other</label>
+  </div>
+</div>
+<script src="<?php out::url('js/filter.js'); ?>"></script>
